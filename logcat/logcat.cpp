ed7c677da052f4db8c8a26db5edef47f4eb4d4c3
Adnan Begovic <adnan@cyngn.com>
2015-10-13 16:28:49 -0700

    logcat: Map '-C' to 'logcat -v color'
    Change-Id: I0419f4551a6dfd77c7d4833050b36da28113ed9b
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -403,8 +403,9 @@ static void show_help(const char *cmd)
                     "                  other pruning activity is oldest first. Special case ~!\n"
                     "                  represents an automatic quicker pruning for the noisiest\n"
                     "                  UID as determined by the current statistics.\n"
-                    "  -P '<list> ...', --prune='<list> ...'\n"
-                    "                  Set prune white and ~black list, using same format as\n"
+                    "  -C              colored output\n"
+                    "  -P '<list> ...' set prune white and ~black list, using same format as\n"
+                    "  --prune='<list> ...'  printed above. Must be quoted.\n"
                     "                  listed above. Must be quoted.\n"
                     "  --pid=<pid>     Only prints logs from the given pid.\n"
                     // Check ANDROID_LOG_WRAP_DEFAULT_TIMEOUT value for match to 2 hours
@@ -657,6 +658,7 @@ int main(int argc, char **argv)
           { "buffer",        required_argument, NULL,   'b' },
           { "buffer-size",   optional_argument, NULL,   'g' },
           { "clear",         no_argument,       NULL,   'c' },
+          { "color",         no_argument,       NULL,   'C' },
           { "dividers",      no_argument,       NULL,   'D' },
           { "file",          required_argument, NULL,   'f' },
           { "format",        required_argument, NULL,   'v' },
@@ -680,7 +682,7 @@ int main(int argc, char **argv)
           { NULL,            0,                 NULL,   0 }
         };
 
-        ret = getopt_long(argc, argv, ":cdDLt:T:gG:sQf:r:n:v:b:BSpP:m:e:w:",
+        ret = getopt_long(argc, argv, ":cdDLt:T:gG:sQf:r:n:v:b:BSpCP:m:e:",
                           long_options, &option_index);
 
         if (ret < 0) {
@@ -837,6 +839,10 @@ int main(int argc, char **argv)
 
             case 'P':
                 setPruneList = optarg;
+            break;
+
+            case 'C':
+                setLogFormat ("color");
             break;
 
             case 'b': {
