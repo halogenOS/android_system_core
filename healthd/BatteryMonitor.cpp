6307f24ee4c7f1f36ebb356bbce4c91bf9cd1d35
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2016-05-24 15:19:05 -0700

    healthd: Add support for HVDCP and Wipower chargers
    HVDCP charger is high voltage DCP chargers. Add support to recognize
    it and treat them as AC power source.
    Also, Wipower charging is a wireless charger where the power transfer
    is via resonance and power control messages are exchanged over BLE.
    Treat Wipower as a wireless charger.
    
    CRs-Fixed: 775241
    Change-Id: Id49bc31111825721ffce5a71c29f79659c5fddf0
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -172,9 +172,11 @@ BatteryMonitor::PowerSupplyType BatteryMonitor::readPowerSupplyType(const String
             { "USB_C", ANDROID_POWER_SUPPLY_TYPE_AC },
             { "USB_PD", ANDROID_POWER_SUPPLY_TYPE_AC },
             { "USB_PD_DRP", ANDROID_POWER_SUPPLY_TYPE_USB },
+            { "USB_HVDCP", ANDROID_POWER_SUPPLY_TYPE_AC },
             { "Wireless", ANDROID_POWER_SUPPLY_TYPE_WIRELESS },
             { "DockBattery", ANDROID_POWER_SUPPLY_TYPE_DOCK_BATTERY },
             { "DockAC", ANDROID_POWER_SUPPLY_TYPE_DOCK_AC },
+            { "Wipower", ANDROID_POWER_SUPPLY_TYPE_WIRELESS },
             { "DASH", ANDROID_POWER_SUPPLY_TYPE_AC },
             { NULL, 0 },
     };
