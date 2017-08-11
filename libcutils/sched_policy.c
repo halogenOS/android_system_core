7c759c3e3daf4f479b7d2d6424910cdde5da9d8b
Simao Gomes Viana <xdevs23@outlook.com>
2017-08-11 14:32:05 +0200

    Revert "libcutils: Fixes for devices with cpusets disabled"
    This reverts commit f933bbbcce28d0826545811216e1085779123a93.
    
    Change-Id: Ia2db22ef728a74450b378d86b736402dee5bebd4

--- libcutils/sched_policy.c
+++ libcutils/sched_policy.c
@@ -110,9 +110,7 @@ static int add_tid_to_cgroup(int tid, int fd)
 }
 
 static void __initialize(void) {
-#if defined(USE_CPUSETS) || defined(USE_SCHEDBOOST)
     char* filename;
-#endif //defined(USE_CPUSETS) || defined(USE_SCHEDBOOST)
     if (!access("/dev/cpuctl/tasks", W_OK)) {
         __sys_supports_schedgroups = 1;
 
@@ -324,12 +322,11 @@ int set_cpuset_policy(int tid, SchedPolicy policy)
         boost_fd = fd = -1;
         break;
     }
-#if defined(USE_CPUSETS) || defined(USE_SCHEDBOOST)
+
     if (add_tid_to_cgroup(tid, fd) != 0) {
         if (errno != ESRCH && errno != ENOENT)
             return -errno;
     }
-#endif //defined(USE_CPUSETS) || defined(USE_SCHEDBOOST)
 
 #ifdef USE_SCHEDBOOST
     if (boost_fd > 0 && add_tid_to_cgroup(tid, boost_fd) != 0) {
