omnirom/android_system_core_new

init: Allow Permissive SELinux by make flag
* Most of us make dumb bringup commits, instead just
  set this as an env var.

Change-Id: I684d7d809605fdf912cbc00f804427412307f180

init/selinux.cpp
@@ -716,6 +716,9 @@ void ReadPolicy(std::string* policy) {
 void SelinuxSetEnforcement() {
     bool kernel_enforcing = (security_getenforce() == 1);
     bool is_enforcing = IsEnforcing();
+#ifndef WITH_SELINUX_PERMISSIVE
+    is_enforcing = false;
+#endif
     if (kernel_enforcing != is_enforcing) {
         if (security_setenforce(is_enforcing)) {
             PLOG(FATAL) << "security_setenforce(" << (is_enforcing ? "true" : "false")

halogenOS/android_system_core

init/selinux.cpp
@@ -747,6 +747,9 @@ void ReadPolicy(std::string* policy) {
 void SelinuxSetEnforcement() {
     bool kernel_enforcing = (security_getenforce() == 1);
     bool is_enforcing = IsEnforcing();
+#ifdef WITH_SELINUX_PERMISSIVE
+    is_enforcing = false;
+#endif
     if (kernel_enforcing != is_enforcing) {
         if (security_setenforce(is_enforcing)) {
             PLOG(FATAL) << "security_setenforce(" << (is_enforcing ? "true" : "false")